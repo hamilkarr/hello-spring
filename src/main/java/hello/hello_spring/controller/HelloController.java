2aadf4fc169332c0ef64ce6111b86ce9c7aa9301
hamilkarr <12uclock@gmail.com>
2025-09-03 11:11:29 +0900

    헬로우 컨트롤러에 문자열 및 객체 반환을 위한 새로운 API 엔드포인트 추가

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -19,6 +20,30 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // 문자열로 반환
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+    
+    @GetMapping("hello-api")
+    @ResponseBody // 문자가 아닌 객체를 반환하면 자동으로 JSON 형식으로 변환하여 반환
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
     
 }
